9b3496f62c99cf65a3d5495f6a2f7fc6681d5e23
3rdPlanetC <o_nongearth_o@hotmail.com>
2021-11-12 22:18:54 +0700

    commit message: added obj data
    commit message: added setObjValue function

---
+++ featureB.js
@@ -0,0 +1,12 @@
+const obj = {
+    name: 'Earth',
+    age: 18
+}
+
+function setObjValue() {
+    return {
+        ...obj,
+        firstName: 'Chonlasai',
+        LastName: 'Attachitvatin'
+    }
+}
